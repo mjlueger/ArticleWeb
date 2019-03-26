650e94113e93a48a52f02b435c5bdf95eb273d61
McKenna <mjlueger@ksu.edu>
2019-03-26 17:21:06 -0500

    Improved website 3/26

---
+++ js/script.js
@@ -0,0 +1,14 @@
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
